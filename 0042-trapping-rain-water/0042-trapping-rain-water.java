d9de745d464c4f6be31688836f9605fbe634a013
Yeji Heo <104667483+yestj@users.noreply.github.com>
2024-02-21 23:48:04 +0900

    Time: 1 ms (73.38%), Space: 46.8 MB (10.08%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,36 +1,18 @@
 class Solution {
     public int trap(int[] height) {
-        int currHeight = height[0];
-        int[][] dp = new int[2][height.length];
-        dp[0][0] = 0;
-        dp[1][height.length - 1] = 0;
-
-        for (int i = 1; i < height.length; i++) {
-            if (height[i] >= currHeight) {
-                dp[0][i] = 0;
-                currHeight = height[i];
-            } else {
-                dp[0][i] = currHeight - height[i];
-            }
-        }
-
-        currHeight = height[height.length - 1];
-        for (int i = height.length - 2; i >= 0; i--) {
-            if (height[i] >= currHeight) {
-                dp[1][i] = 0;
-                currHeight =height[i];
-            } else {
-                dp[1][i] = currHeight - height[i];
-            }
-        }
-
+        
+        int leftIdx = 0, rightIdx= height.length - 1;
+        int leftMax = Integer.MIN_VALUE, rightMax = Integer.MIN_VALUE;
         int result = 0;
 
-        for (int i = 0; i < height.length; i++) {
-            result += Math.min(dp[0][i], dp[1][i]);
+        while (leftIdx < rightIdx) {
+            leftMax = Math.max(leftMax, height[leftIdx]);
+            rightMax = Math.max(rightMax, height[rightIdx]);
+            result += (leftMax < rightMax) ? leftMax - height[leftIdx++] : rightMax - height[rightIdx--];
         }
 
         return result;
+
     }
 
 }
