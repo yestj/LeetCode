88bf9ea89dc260a9d57e70b1bf2ed036cf15a144
Yeji Heo <104667483+yestj@users.noreply.github.com>
2024-03-11 22:49:17 +0900

    Time: 1 ms (100.00%), Space: 44 MB (94.23%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,23 +10,41 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        List<Integer> mergeList = new ArrayList<>();
-        for (ListNode l : lists) {
-            ListNode curr = l;
-            while (curr != null) {
-                mergeList.add(curr.val);
-                curr = curr.next;
-            }
-        }
-        Collections.sort(mergeList);
         
-        ListNode ans = new ListNode();
-        ListNode tmp = ans;
-        for (int i : mergeList) {
-            tmp.next = new ListNode(i);
-            tmp = tmp.next;
+        if (lists == null || lists.length == 0) return null;
+
+        return mergeLists(lists, 0, lists.length - 1);
+    }
+
+    private ListNode mergeLists(ListNode[] lists, int start, int end) { 
+        
+        if (start == end) return lists[start];
+        
+        if (start + 1 == end) return merge(lists[start], lists[end]);
+
+        int mid = start + (end - start) / 2;
+        ListNode left = mergeLists(lists, start, mid);
+        ListNode right = mergeLists(lists, mid + 1, end);
+        return merge (left, right);
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode tmp = new ListNode(0);
+        ListNode curr = tmp;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
         }
 
-        return ans.next;
+        curr.next = (l1 != null) ? l1 : l2;
+
+        return tmp.next;
     }
 }
